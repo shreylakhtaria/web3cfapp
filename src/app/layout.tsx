05b0a36955a8eb7b7c55a6ba0d101f67c74c8eed
shreylakhtaria <shrey.lakhataria.code@gmail.com>
2025-02-24 22:44:25 +0530

    changes made

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
+import Navbar from "@/app/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
+  title: "CrowdFunding App",
   description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+    "This is CrowdFunding App",
 };
 
 export default function RootLayout({
@@ -18,8 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThirdwebProvider>{children}</ThirdwebProvider>
+      <body className="bg-slate-100 text-slate-700">
+        <ThirdwebProvider>
+          <Navbar />
+          {children}
+        </ThirdwebProvider>
       </body>
     </html>
   );
